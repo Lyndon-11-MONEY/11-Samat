a22a768b8081a53d51b8008ba0fb80045028b5e0
Lyndon-11-MONEY <lfnicolas2030@clc.pshs.edu.ph>
2026-09-25 06:32:43 +0800

    Add Hero class and instantiate characters

--- q1/q1_sg5_a1_samat_nicolas.py
+++ q1/q1_sg5_a1_samat_nicolas.py
@@ -1,14 +1,19 @@
+"""
+#11 Nicolas,Lyndon John F.
+9-Samat
+25/09/26
+"""
+
+
 class Hero:
     def __init__(self, name, hp):
         self.name=name
         self.hp=hp
-
     def take_damage(self, amount):
         self.hp-=amount
-
+        
 arthur=Hero("Arthur", 100)
 morgana=Hero("Morgana", 100)
-
 arthur.take_damage(10)
 
 print(arthur.name,"HP:",arthur.hp)
